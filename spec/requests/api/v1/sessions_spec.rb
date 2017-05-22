8db0120eac31c51d3a3799ec024e6d14599925b6
Josano Soares <josano@hotmail.com>
2017-05-22 16:17:04 +0000

    Adicionando a action destroy na session controller com testes

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -35,9 +35,28 @@ require 'rails_helper'
         expect(response).to have_http_status(401)
       end
 
-      it 'returns the json data for the errors with auth token' do
+      it 'returns the json data for the errors' do
+        user.reload
         expect(json_body).to have_key(:errors)
       end
     end    
   end 
+
+
+  describe 'DELETE /sessions/:id' do
+    let(:auth_token) { user.auth_token }
+
+    before do
+      delete "/sessions/#{auth_token}", params: {}, headers: headers
+    end
+
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+
+    it 'changes the user auth token' do
+      expect(User.find_by(auth_token: auth_token)).to be_nil
+    end
+  end
+
 end
